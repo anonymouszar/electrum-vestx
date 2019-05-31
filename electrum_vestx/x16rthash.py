294c074aaefdac5db7b88e3bdce590499d797ebf
anonymouszar <anonymouszarsa@gmail.com>
2019-05-31 11:47:45 +0000

    Initial commit

---
+++ electrum_vestx/x16rthash.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+
+try:
+    from x16rt_hash import getPoWHash
+    import_success = True
+    load_libx16rthash = False
+except ImportError:
+    import_success = False
+    load_libx16rthash = True
+
+
+if load_libx16rthash:
+    from ctypes import cdll, create_string_buffer, byref
+
+    if sys.platform == 'darwin':
+        name = 'libx16rthash.dylib'
+    elif sys.platform in ('windows', 'win32'):
+        name = 'libx16rthash-0.dll'
+    else:
+        name = 'libx16rthash.so'
+
+    try:
+        lx16rthash = cdll.LoadLibrary(name)
+        x16rt_hash = lx16rthash.x16rt_hash
+    except:
+        load_libx16rthash = False
+
+
+if load_libx16rthash:
+    hash_out = create_string_buffer(32)
+
+    def getPoWHash(header):
+        x16rt_hash(header, byref(hash_out))
+        return hash_out.raw
+
+
+if not import_success and not load_libx16rthash:
+    raise ImportError('Can not import x16rt_hash')
