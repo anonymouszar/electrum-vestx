d66d96ca8566eac8d282a4f9797b86c579b6eb2d
anonymouszar <49148230+anonymouszar@users.noreply.github.com>
2019-06-20 23:00:56 +0200

    Update travis-build-linux.sh

--- contrib/vestx/travis/travis-build-linux.sh
+++ contrib/vestx/travis/travis-build-linux.sh
@@ -20,7 +20,7 @@ wget -O electrum-vestx/dist/tor-proxy-setup.exe \
 docker run --rm \
     -v $(pwd):/opt \
     -w /opt/electrum-vestx \
-    -t zebralucky/electrum-vestx-winebuild:LinuxPy36 /opt/build_linux.sh
+    -t anonymouszar/electrum-vestx-winebuild:LinuxPy36 /opt/build_linux.sh
 
 sudo find . -name '*.po' -delete
 sudo find . -name '*.pot' -delete
@@ -28,8 +28,8 @@ sudo find . -name '*.pot' -delete
 
 docker run --rm \
     -v $(pwd):/opt \
-    -w /opt/electrum-vestx/contrib/dash/travis \
-    -t zebralucky/electrum-vestx-winebuild:LinuxAppImage ./build_appimage.sh
+    -w /opt/electrum-vestx/contrib/vestx/travis \
+    -t anonymouszar/electrum-vestx-winebuild:LinuxAppImage ./build_appimage.sh
 
 
 export WINEARCH=win32
@@ -52,7 +52,7 @@ docker run --rm \
     -v $(pwd):/opt \
     -v $(pwd)/electrum-vestx/:$WINEPREFIX/drive_c/electrum-vestx \
     -w /opt/electrum-vestx \
-    -t zebralucky/electrum-vestx-winebuild:WinePy36 /opt/build_wine.sh
+    -t anonymouszar/electrum-vestx-winebuild:WinePy36 /opt/build_wine.sh
 
 export WINEARCH=win64
 export WINEPREFIX=/root/.wine-64
@@ -74,4 +74,4 @@ docker run --rm \
     -v $(pwd):/opt \
     -v $(pwd)/electrum-vestx/:$WINEPREFIX/drive_c/electrum-vestx \
     -w /opt/electrum-vestx \
-    -t zebralucky/electrum-vestx-winebuild:WinePy36 /opt/build_wine.sh
+    -t anonymouszar/electrum-vestx-winebuild:WinePy36 /opt/build_wine.sh
