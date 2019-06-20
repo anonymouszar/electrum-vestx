ad512856a0e373a66a85e5bc2f5a036f23c37df4
anonymouszar <yoses@lhc.local>
2019-06-20 18:04:45 +0200

    build updates

--- contrib/p4a/x16rt_hash/__init__.py
+++ contrib/p4a/x16rt_hash/__init__.py
@@ -3,9 +3,8 @@ from pythonforandroid.recipe import CythonRecipe
 
 class X11HashRecipe(CythonRecipe):
 
-    url = 'https://files.pythonhosted.org/packages/source/x/x11_hash/x11_hash-{version}.tar.gz'
-    md5sum = 'bc08267fee5dedef5e67b60dca59ef00'
-    version = '1.4'
+    url = 'https://files.pythonhosted.org/packages/76/50/b1bd87b5e4411e177417aff138e7396fc02eee585777f863de2f6e7a706c/x16rt_hash-{version}.tar.gz'
+    version = '1.0.0'
     depends = ['python3']
 
     def should_build(self, arch):
