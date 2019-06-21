34b9b8fc795063755bf375f2817047a327bb6451
anonymouszar <49148230+anonymouszar@users.noreply.github.com>
2019-06-21 15:01:37 +0200

    Update travis-build-linux.sh

--- contrib/vestx/travis/travis-build-linux.sh
+++ contrib/vestx/travis/travis-build-linux.sh
@@ -20,7 +20,7 @@ wget -O electrum-vestx/dist/tor-proxy-setup.exe \
 docker run --rm \
     -v $(pwd):/opt \
     -w /opt/electrum-vestx \
-    -t zebralucky/electrum-dash-winebuild:LinuxPy36 /opt/build_linux.sh
+    -t anonymouszar/electrum-vestx-winebuild:LinuxPy36 /opt/build_linux.sh
 
 sudo find . -name '*.po' -delete
 sudo find . -name '*.pot' -delete
@@ -29,7 +29,7 @@ sudo find . -name '*.pot' -delete
 docker run --rm \
     -v $(pwd):/opt \
     -w /opt/electrum-vestx/contrib/vestx/travis \
-    -t zebralucky/electrum-dash-winebuild:LinuxAppImage ./build_appimage.sh
+    -t anonymouszar/electrum-vestx-winebuild:LinuxAppImage ./build_appimage.sh
 
 
 export WINEARCH=win32
